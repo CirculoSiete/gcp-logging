7a0c103e2ce553bf5533715bf6e3bd8b68c9fd38
Domingo Suarez Torres <domingo.suarez@gmail.com>
2019-03-15 23:08:01 -0600

    mejoras menores al codigo

--- src/main/java/com/circulosiete/gcp/log/extractor/api/LogController.java
+++ src/main/java/com/circulosiete/gcp/log/extractor/api/LogController.java
@@ -1,8 +1,7 @@
 package com.circulosiete.gcp.log.extractor.api;
 
+import com.circulosiete.gcp.log.extractor.db.LogRequestRepository;
 import com.circulosiete.gcp.log.extractor.model.LogRequestCommand;
-import org.apache.commons.lang3.StringUtils;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,10 +16,10 @@ import static org.springframework.http.HttpStatus.ACCEPTED;
 @RestController
 public class LogController {
 
-  private final JdbcTemplate template;
+  private final LogRequestRepository repository;
 
-  public LogController(JdbcTemplate template) {
-    this.template = template;
+  public LogController(LogRequestRepository repository) {
+    this.repository = repository;
   }
 
   @ResponseStatus(ACCEPTED)
@@ -30,17 +29,7 @@ public class LogController {
     String stackdriver_log_name = Optional.ofNullable(logRequest.getLogName()).orElse(System.getenv("STACKDRIVER_LOG_NAME"));
     logRequest.setLogName(stackdriver_log_name);
 
-    if (!StringUtils.isNotBlank(stackdriver_log_name)) {
-      throw new RuntimeException("El nombre del log es requerido.");
-    }
-
-    Optional.ofNullable(logRequest.getNotifyTo())
-      .orElseThrow(() -> new RuntimeException("Es requerido el email para enviar la notificacion"));
-
-    //TODO: validar multiples solicitudes del mismo log en un periodo corto de tiempo
-    String insert = "insert into log_requests (log_name, items, notify_to, notification_media) values (?,?,?,?)";
-    template.update(insert,
-      stackdriver_log_name, logRequest.getItems(), logRequest.getNotifyTo(), logRequest.getNotificationMedia());
+    repository.insert(logRequest);
 
     return Collections.singletonMap("created", true);
   }
--- src/main/java/com/circulosiete/gcp/log/extractor/db/LogRequestRepository.java
+++ src/main/java/com/circulosiete/gcp/log/extractor/db/LogRequestRepository.java
@@ -1,13 +1,17 @@
 package com.circulosiete.gcp.log.extractor.db;
 
 import com.circulosiete.gcp.log.extractor.model.LogRequest;
+import com.circulosiete.gcp.log.extractor.model.LogRequestCommand;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class LogRequestRepository {
+  public static final String INSERT_LOG_REQUEST = "insert into log_requests (log_name, items, notify_to, notification_media) values (?,?,?,?)";
   public static final String NEW_LOG_REQUESTS = "select id from log_requests where status='NEW'";
   public static final String SINGLE_NEW_LOG_REQUEST = "select * from log_requests where ID = ? and status = ?";
   public static final String UPDATE_TO_DONE_NEW_LOG_REQUEST = "update log_requests set status = ? where id = ? and status = ?";
@@ -15,6 +19,23 @@ public class LogRequestRepository {
 
   public LogRequestRepository(JdbcTemplate template) {
     this.template = template;
+  }
+
+  public void insert(LogRequestCommand logRequest) {
+    if (!StringUtils.isNotBlank(logRequest.getLogName())) {
+      throw new RuntimeException("El nombre del log es requerido.");
+    }
+
+    Optional.ofNullable(logRequest.getNotifyTo())
+      .orElseThrow(() -> new RuntimeException("Es requerido el email para enviar la notificacion"));
+    //TODO: validar multiples solicitudes del mismo log en un periodo corto de tiempo
+
+    template.update(
+      INSERT_LOG_REQUEST,
+      logRequest.getLogName(),
+      logRequest.getItems(),
+      logRequest.getNotifyTo(),
+      logRequest.getNotificationMedia());
   }
 
   public List<Long> newLogRequests() {
@@ -36,6 +57,8 @@ public class LogRequestRepository {
   }
 
   public void update(Long id) {
-    template.update(UPDATE_TO_DONE_NEW_LOG_REQUEST, "DONE", id, "NEW");
+    template.update(
+      UPDATE_TO_DONE_NEW_LOG_REQUEST,
+      "DONE", id, "NEW");
   }
 }
